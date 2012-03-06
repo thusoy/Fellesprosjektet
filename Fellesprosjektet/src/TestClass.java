dadd33fe60241531abe219316a4b1897f988c87b
Tarjei Husoy <tarjei@roms.no>
2012-03-06 09:53:46 +0100

    La inn rammeverk

--- Fellesprosjektet/src/TestClass.java
+++ Fellesprosjektet/src/TestClass.java
@@ -2,9 +2,10 @@
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
+		init();
 	}
 	
-	public void init(){
+	public static void init(){
 		System.out.println("Test 2!");
 	}
 }
