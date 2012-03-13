34e481d07ee4c9884afda9d8960b55d9d1737dad
Haakon Garseg Mork <haakongmork@gmail.com>
2012-03-13 14:05:41 +0100

    opprettet klassen Message med sine attributter og metoden showMessage()

---
+++ Fellesprosjektet/src/calendar/Message.java
@@ -0,0 +1,24 @@
+package calendar;
+
+import java.util.Date;
+import no.ntnu.fp.model.Person;
+
+public class Message {
+	
+	private int msgId;
+	private Date dateSent;
+	private String content;
+	private String title;
+	
+	public Message (String title, String content, Date dateSent){
+		this.title = title;
+		this.content = content;
+		this.dateSent = dateSent;
+	}
+	
+	public String showMessage(Person user){
+		String query = "UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%s' AND msgId='%s'";
+		executeQuery(String.format(query, user.getId(), this.msgId));
+		return content;
+	}
+}
