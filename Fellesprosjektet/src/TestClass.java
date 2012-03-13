4906f2b47d8ac20969f09351e215da37d44e47f3
mariann <mariann@78.91.10.210>
2012-03-13 11:25:09 +0100

    La inn test

--- Fellesprosjektet/src/TestClass.java
+++ Fellesprosjektet/src/TestClass.java
@@ -1,11 +1,6 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello, Git!");
-		init();
-	}
-	
-	public static void init(){
-		System.out.println("Test 2!");
+		System.out.println("HellO!");
 	}
 }
