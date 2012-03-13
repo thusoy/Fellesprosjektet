5ee4956149f66db1c6d8e5b8059a7ce5f19d0e4d
Haakon Garseg Mork <haakongmork@gmail.com>
2012-03-13 16:26:10 +0100

    Room --> Laget en ny metode avaiableRooms som returnerer en liste med hvilke rom som er ledig, til angitte tidspunkt. availableRooms benytter seg av isValid, som går igjennom en HashMap med alle tidspunkt et rom er opptatt.

--- Fellesprosjektet/src/calendar/Room.java
+++ Fellesprosjektet/src/calendar/Room.java
@@ -1,10 +1,14 @@
 package calendar;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
 
-public class Room {
+public class Room extends DBObject{
 	private String name;
 	private int capacity;
+	private LinkedHashMap<Date, Date> isOccupied;
 	
 	public String getName() {
 		return name;
@@ -18,9 +22,25 @@ public class Room {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	public boolean isAvailable(Date start, Date end, int capacity){
-//	implementer
+	public static List<Room> availableRooms(Date start, Date end, int capacity){
+		List<Room> rooms = new ArrayList<Room>();
+		for(Room room: Room.all()) {
+			if (room.isValid(start, end, capacity)) {
+				rooms.add(room);
+			}
+		}
 		
+		return rooms;
+	}
+	private boolean isValid(Date startCandidate, Date endCandidate, int capacity) {
+		for (Date start : isOccupied.keySet()) {
+			Date end = isOccupied.get(start);
+			if (!(startCandidate.before(start) || startCandidate.after(end)) || 
+					!(endCandidate.before(start) || endCandidate.after(end))) {
+				return false;
+			}			
+		}		
+		return true;
 	}
 
 }
