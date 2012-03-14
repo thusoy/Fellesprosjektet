96fa8207594e6619b0c97ea14de554456ab5891c
Haakon Garseg Mork <haakongmork@gmail.com>
2012-03-14 10:31:46 +0100

    laget de fleste sql spørringene for Appointment

--- Fellesprosjektet/src/server/SQLQueries.java
+++ Fellesprosjektet/src/server/SQLQueries.java
@@ -1,5 +1,6 @@
 package server;
 
+
 public class SQLQueries {
 	
 	/**
@@ -11,20 +12,20 @@ public class SQLQueries {
 			"UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%i' AND msgId='%i'";
 	String queryGetContentMsg = 
 			"SELECT content FROM Message WHERE msgId='%i'";
+	
+	//Appointment sp¿rringer
 	/**
-	 * APPOINTMENT : query for Œ legge tl en appointment
+	 * Query for Œ legge tl en appointment
 	 * brukes til funksjonen save()
-	 * rekkef¿lge--> id, place, title, descr, start, end, daysAppearing, endOfRe,
-	 * isPrivate, creatorId, participants, room_name 
+	 * Hvilke users som blir invitert, blir evt lagt til med 'queryAddUserToAppointment'
+	 * som ligger lengre nede i dokumentet.
+	 * rekkef¿lge--> id (settes selv), place, title, descr, start, end, 
+	 * daysAppearing, endOfRe, isPrivate, creatorId, participants, room_name 
 	 */
-	//IKKE FERDIG
-	public void createString(Set<Day> daysAppearing)){
-		
-	}
+
 	String querySaveAppointment = 
-			"INSERT INTO Appointment VALUES(%i, %s, %s, %s, date, date, set, date" +
-			"%b, %i)";
-	//mangler hashmap om room_name
+			"INSERT INTO Appointment VALUES(%s, %s, %s, " +
+			"date, date, set, Date,'%s' ,%b, %i)";
 	/**
 	 * UPDATE a meeting
 	 * Oppdaterer et allerede opprettet m¿te (appointment), 
@@ -32,7 +33,6 @@ public class SQLQueries {
 	 */
 	String queryUpdateAppointment =
 			"UPDATE Appointment SET" +
-			" appId='%i'" +
 			" place='%s'" +
 			" title='%s'" +
 			" description='%s'" +
@@ -40,12 +40,67 @@ public class SQLQueries {
 			" endTime='date'" +
 			" daysAppearing='set'" +
 			" endOfRepeatDate='date'" +
+			" roomName='%s'" +
 			" isPrivate='%b'" +
 			" creatorId='%i'" +
 			" WHERE appId='%i'";
+	/**
+	 * getAppointment()
+	 */
+	String queryGetAppointment =
+			"SELECT * FROM Appointment WHERE appId='%i'";
+	/**
+	 * getTitle()
+	 */
+	String queryGetTitleAppointment =
+			"SELECT title FROM Appointment WHERE appId='%i'";
+	/**
+	 * getPlace()
+	 */
+	String queryGetPlaceAppointment =
+			"SELECT place FROM Appointment WHERE appId='%i'";
+	/**
+	 * getRoom()
+	 */
+	String queryGetRoomAppointment =
+			"SELECT roomName FROM Appointment WHERE appId='%i'";
+	/**
+	 * getStart()
+	 */
+	String queryGetStartAppointment =
+			"SELECT startTime FROM Appointment WHERE appId='%i'";
+	/**
+	 * getEnd()
+	 */
+	String queryGetEndAppointment =
+			"SELECT endTime FROM Appointment WHERE appId='%i'";
+	/**
+	 * deleteAppointment()
+	 */
+	String queryDeleteAppointment =
+			"DELETE FROM Appointment WHERE appId='%i'";
+	/**
+	 * acceptInvite(user, boolean)
+	 */
 	String queryUpdateUserAppointment = 
 			"UPDATE UserAppointment SET" +
 			" hasAccepted='%b'" +
 			" WHERE userId='%i' AND msgId='%i'";
+	/**
+	 * getParticipants()
+	 */
+	String queryGetParticipants =
+			"SELECT userId, hasAccepted FROM UserAppointment" +
+			" WHERE appId='%i'";
+	/**
+	 * updateParticipants(HashMap)
+	 * en funksjon for Œ legge til, og en for Œ slette
+	 * rekkef¿lge --> userId, appId, hasAccepted
+	 */
+	String queryAddUserToAppointment =
+			"INSERT INTO UserAppointment VALUES(%i, %i, %b)";
+	String queryDeleteUserFromAppointment = 
+			"DELETE FROM UserAppoinment WHERE userId='%i' AND msgId='%i'";
+	
 	
 }
