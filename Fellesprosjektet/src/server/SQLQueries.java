b590d07d7311ea4a8368c2c8b9c1d6a6011f5f49
Haakon Garseg Mork <haakongmork@gmail.com>
2012-03-14 09:06:12 +0100

    laget spørringer for Appointment

--- Fellesprosjektet/src/server/SQLQueries.java
+++ Fellesprosjektet/src/server/SQLQueries.java
@@ -8,23 +8,44 @@ public class SQLQueries {
 	 * RETURN content
 	 */
 	String queryReadMessage = 
-			"UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%s' AND msgId='%s'";
+			"UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%i' AND msgId='%i'";
 	String queryGetContentMsg = 
-			"SELECT content FROM message WHERE msgId='%i'";
+			"SELECT content FROM Message WHERE msgId='%i'";
 	/**
 	 * APPOINTMENT : query for Œ legge tl en appointment
 	 * brukes til funksjonen save()
 	 * rekkef¿lge--> id, place, title, descr, start, end, daysAppearing, endOfRe,
-	 * isPrivate, creator, participants, room_name 
+	 * isPrivate, creatorId, participants, room_name 
 	 */
 	//IKKE FERDIG
 	public void createString(Set<Day> daysAppearing)){
 		
 	}
 	String querySaveAppointment = 
-			"INSERT INTO appointment VALUES(%i, %s, %s, %s, date, date, set, date" +
-			"%b, person, hashmap, %s)";
+			"INSERT INTO Appointment VALUES(%i, %s, %s, %s, date, date, set, date" +
+			"%b, %i)";
+	//mangler hashmap om room_name
+	/**
+	 * UPDATE a meeting
+	 * Oppdaterer et allerede opprettet m¿te (appointment), 
+	 * med alle attributter. 
+	 */
 	String queryUpdateAppointment =
-			"";
+			"UPDATE Appointment SET" +
+			" appId='%i'" +
+			" place='%s'" +
+			" title='%s'" +
+			" description='%s'" +
+			" startTime='date'" +
+			" endTime='date'" +
+			" daysAppearing='set'" +
+			" endOfRepeatDate='date'" +
+			" isPrivate='%b'" +
+			" creatorId='%i'" +
+			" WHERE appId='%i'";
+	String queryUpdateUserAppointment = 
+			"UPDATE UserAppointment SET" +
+			" hasAccepted='%b'" +
+			" WHERE userId='%i' AND msgId='%i'";
 	
 }
