1ba781ea93aab5beabaa7815d5149b71ffd03f2d
Tarjei Husoy <tarjei@roms.no>
2012-03-14 18:37:21 +0100

    Oppdaterte testen til å teste individuelle dager, oppdaterte klassen til å støtte det.

--- Fellesprosjektet/src/calendar/Day.java
+++ Fellesprosjektet/src/calendar/Day.java
@@ -1,21 +1,45 @@
 package calendar;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public enum Day{
+	
 	MONDAY("mandag"), TUESDAY("tirsdag"), WEDNESDAY("onsdag"), 
 	THURSDAY("torsdag"), FRIDAY("fredag"), SATURDAY("lørdag"), SUNDAY("søndag");
 	
-	String norwegianRepr;
+	private String norwegian;
 	
 	Day(String norwegian){
-		this.norwegianRepr = norwegian;
+		this.norwegian = norwegian;
 	}
 	
-	public Day fromString(String day){
-		for(Day d: Day.values()){
-			if (d.norwegianRepr.equalsIgnoreCase(day)){
+	public String getNorwegian(){
+		return norwegian;
+	}
+	
+	public static Day fromString(String day){
+		for (Day d: Day.values()){
+			if (d.name().equalsIgnoreCase(day)){
 				return d;
 			}
 		}
-		throw new IllegalArgumentException("No such day!");
+		throw new IllegalArgumentException("Invalid day: " + day);
+	}
+	
+	public static Set<Day> fromSetString(String stringFromSet){
+		if (stringFromSet == null || stringFromSet.length() < 2){
+			return new TreeSet<Day>();
+		}
+		String days = stringFromSet.substring(1, stringFromSet.length()-1); 
+		Set<Day> output = new TreeSet<Day>();
+		String[] dayArray = days.split(", ");
+		for(String dayString: dayArray){
+			try{
+				Day day = Day.fromString(dayString);
+				output.add(day);
+			} catch (IllegalArgumentException e){}
+		}
+		return output;
 	}
 }
--- Fellesprosjektet/tests/calendar/TestDay.java
+++ Fellesprosjektet/tests/calendar/TestDay.java
@@ -1,14 +1,13 @@
 package calendar;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Test;
-
-import calendar.Day;
 
 public class TestDay {
 
@@ -24,40 +23,61 @@ public class TestDay {
 	}
 	
 	@Test
-	public void testCorrectFromString() {
-		String teststring = "MONDAY, TUESDAY";
-		Set<Day> days = Day.fromString(teststring);
+	public void testFromString(){
+		String teststring = "MONDAY";
+		assertEquals(Day.MONDAY, Day.fromString(teststring));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testFromStringEmpty(){
+		String teststring = "";
+		Day.fromString(teststring);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testFromStringNull(){
+		String teststring = null;
+		Day.fromString(teststring);
+	}
+	
+	@Test
+	public void testCorrectFromSetString() {
+		TreeSet<Day> set = new TreeSet<Day>();
+		set.add(Day.MONDAY);
+		set.add(Day.TUESDAY);
+		String teststring = set.toString();
+		Set<Day> days = Day.fromSetString(teststring);
 		assertEquals("Det skal være to elementer i days etter å ha testet med strengen " +
-				"'MONDAY, TUESDAY'", days.size(), 2);
+				"'MONDAY, TUESDAY'", 2, days.size());
 		assertTrue("Mandag må ligge i settet som returneres", days.contains(Day.MONDAY));
 		assertTrue("Tirsdag må ligge i settet som returneres", days.contains(Day.TUESDAY));
 	}
 	
 	@Test
-	public void testWrongFromString(){
+	public void testWrongFromSetString(){
 		String teststring = "LUNDI, MERCREDI, ";
-		Set<Day> days = Day.fromString(teststring);
-		assertEquals("Med ugyldig input skal settet være tomt!", days.size(), 0);
+		Set<Day> days = Day.fromSetString(teststring);
+		assertEquals("Med ugyldig input skal settet være tomt!", 0, days.size());
 	}
 	
 	@Test
-	public void testWrongFromString2(){
+	public void testWrongFromSetString2(){
 		String teststring = "Dette er en lengre bullshittekst om ingenting!";
-		Set<Day> days = Day.fromString(teststring);
-		assertEquals("Med ugyldig input skal settet være tomt!", days.size(), 0);
+		Set<Day> days = Day.fromSetString(teststring);
+		assertEquals("Med ugyldig input skal settet være tomt!", 0, days.size());
 	}
 	
 	@Test
-	public void testEmptyFromString(){
+	public void testEmptyFromSetString(){
 		String teststring = "";
-		Set<Day> days = Day.fromString(teststring);
-		assertEquals("Med tom input skal settet være tomt!", days.size(), 0);
+		Set<Day> days = Day.fromSetString(teststring);
+		assertEquals("Med tom input skal settet være tomt!", 0, days.size());
 	}
 	
 	@Test
-	public void testNullFromString(){
+	public void testNullFromSetString(){
 		String teststring = null;
-		Set<Day> days = Day.fromString(teststring);
-		assertEquals("Med null-input skal settet være tomt!", days.size(), 0);
+		Set<Day> days = Day.fromSetString(teststring);
+		assertEquals("Med null-input skal settet være tomt!", 0, days.size());
 	}
 }
