b1f0cb57c611ab770b00fd298edc8826ee0f8ca3
mariann <mariann@78.91.7.155>
2012-03-14 13:51:21 +0100

    Opprettet MessageHandler og endret litt i Message

--- Fellesprosjektet/src/calendar/Message.java
+++ Fellesprosjektet/src/calendar/Message.java
@@ -1,6 +1,10 @@
 package calendar;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Date;
+
+import server.MessageHandler;
 import no.ntnu.fp.model.Person;
 
 public class Message extends DBObject {
@@ -11,16 +15,34 @@ public class Message extends DBObject {
 	private String title;
 	
 	public Message (String title, String content, Date dateSent){
-		this.title = title;
-		this.content = content;
-		this.dateSent = dateSent;
+		this.title=title;
+		this.content=content;
+		this.dateSent=dateSent;
 	}
 	
-	public String showMessage(Person user){
-		return getMsg(this, user);
+	public String showMessage(Message msgId, Person user) throws ClassNotFoundException, IOException, SQLException{
+		return MessageHandler.getContentMessage(msgId, user) ;
 	}
 	
 	public long getId() {
 		return msgId;
 	}
+
+	public Date getDateSent() {
+		return dateSent;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
 }
