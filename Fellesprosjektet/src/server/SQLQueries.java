677f30223b4736076323d2b0ccd9e53b2dc7f231
mariann <mariann@78.91.7.155>
2012-03-14 11:34:20 +0100

    Lagt inn SQL-spørringer til klasse Message, Person, Room og Calendar.

--- Fellesprosjektet/src/server/SQLQueries.java
+++ Fellesprosjektet/src/server/SQLQueries.java
@@ -8,12 +8,8 @@ public class SQLQueries {
 	 * trenger parametrene userId og msgId
 	 * RETURN content
 	 */
-	String queryReadMessage = 
-			"UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%i' AND msgId='%i'";
-	String queryGetContentMsg = 
-			"SELECT content FROM Message WHERE msgId='%i'";
 	
-	//Appointment sp¿rringer
+	//Appointment spørringer
 	/**
 	 * Query for Œ legge tl en appointment
 	 * brukes til funksjonen save()
@@ -102,5 +98,112 @@ public class SQLQueries {
 	String queryDeleteUserFromAppointment = 
 			"DELETE FROM UserAppoinment WHERE userId='%i' AND msgId='%i'";
 	
+//	Spørringer til klasse Person:
+
+	// lagrer en ny bruker i systemet:
+	String querySavePerson = 
+				"INSERT INTO Person VALUES(%i, %s, %s, %s, %s, %s)";
+
+	// oppdaterer bruker
+	String queryUpdatePerson =
+				"UPDATE Person SET" +
+				" userId='%i'" +
+				" email='%s'" +
+				" passwordHash='%s'" +
+				" firstname='%s'" +
+				" lastname='%s'" +
+				" department='%s'";
+
+	// slette bruker
+	String queryDeletePerson =
+				"DELETE FROM Person WHERE userId='%i'";
+
+				
+	// hente userId
+	String queryGetuserId = 
+				"SELECT userId FROM Person WHERE userId='%i'";
+
+	// hente email
+	String queryGetemail = 
+				"SELECT email FROM Person WHERE userId='%i'";
+
+	// hente firstname
+	String queryGetfirstname = 
+				"SELECT firstname FROM Person WHERE userId ='%i'";
+
+	// hente lastname
+	String queryGetlastname = 
+				"SELECT lastname FROM Person WHERE userId ='%i'";
+
+	// hente lastname
+	String queryGetlastname = 
+				"SELECT lastname FROM Person WHERE userId ='%i'";
+
+	// hente department
+	String queryGetdepartment = 
+				"SELECT department FROM Person WHERE userId ='%i'";
+
+	// hente lastname
+	String queryGetlastname = 
+				"SELECT lastname FROM Person WHERE userId ='%i'";
+
+
+//	Spørringer til klasse Calendar:
+
+	// lagrer en ny kalender i systemet:
+	String querySaveCalendar = 
+				"INSERT INTO Calendar”;
+
+	//queryGetAppointments = 
+				“Select * FROM Appointments WHERE startTime==start && endTime==end”;
+
+
+//	Spørringer til klasse UserCalendars:
+
+	// 
+
+//	Spørringer til klasse Room:
+
+	// oppretter nytt rom med navn og kapasitet:
+	String querySaveRoom = 
+				"INSERT INTO Room VALUES(%s, %i)";
+
+	// oppdaterer rom
+	String queryUpdateRoom =
+				"UPDATE Room SET" +
+				" name='%s'" +
+				" capacity='%s'";
+
+
+	// hente romnavn
+	String queryGetroomName = 
+				"SELECT name FROM Room WHERE name='%s'";
+
+	// hente romkapasitet
+	String queryGetroomCapacity = 
+				"SELECT capacity FROM Room WHERE name='%i'";
+
+
+//	Spørringer til klasse Message:
+	/**
+	 * MESSAGE : query for Œ oppdatere hvem som har lest et Message-objekt.
+	 * trenger parametrene userId og msgId
+	 * RETURN content
+	 */
+	// Registrere at melding er lest
+	String queryReadMessage = 
+				"UPDATE UserMessages SET hasBeenRead='true' WHERE userId='%i' AND msgId='%i'";
+
+	// Hente ut innholdet/lese en melding
+	String queryGetContentMsg = 
+				"SELECT content FROM Message WHERE msgId='%i'";
+
+	// Opprette en ny melding
+	String querySaveMessage = 
+				"INSERT INTO Message VALUES(%i, Date, %s, %s)”;
+
+	// Hente ut innholdet meldingstittel
+	String queryGetTitle = 
+				"SELECT title FROM Message WHERE msgId='%i'";
 	
 }
