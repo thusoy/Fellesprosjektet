b4247a4d1cb478832708d0d1f3fcbf98612270e2
Tarjei Husoy <tarjei@roms.no>
2012-03-15 15:27:06 +0100

    Minor fix.

--- Fellesprosjektet/src/server/SQLQueries.java
+++ Fellesprosjektet/src/server/SQLQueries.java
@@ -156,7 +156,6 @@ public class SQLQueries {
 	
 	String queryDeleteUserFromCalendar = 
 				"DELETE FROM UserCalendar WHERE userId='%i' AND calendarId='%i'";
-				
 
 //	Spørringer til klasse Room:
 
