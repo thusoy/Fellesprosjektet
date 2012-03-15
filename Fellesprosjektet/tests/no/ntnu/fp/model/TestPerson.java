329759063a5c6e78e7f44b208b22cc6ccf8063fc
Haakon Garseg Mork <haakongmork@gmail.com>
2012-03-15 13:58:09 +0100

    testPerson

---
+++ Fellesprosjektet/tests/no/ntnu/fp/model/TestPerson.java
@@ -0,0 +1,24 @@
+package no.ntnu.fp.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPerson {
+
+	@Test
+	public void test() {
+		Person p = new Person("john", "high", "lol", "komtek", "banan");
+		String johnHash = p.getPasswordHash();
+		Person p2 = new Person("john", "high", "lol", "komtek", "banan");
+		String otherHash = p2.getPasswordHash();
+		System.out.println(johnHash);
+		System.out.println(otherHash);
+		assertFalse("Hashene skal ikke v¾re like!", johnHash.equals(otherHash));
+		p.setPasswordHash("banan");
+		String secondHash = p.getPasswordHash();
+		assertEquals("Samme passord skal ha samme hash pA samme person", secondHash, johnHash);
+		
+	}
+
+}
