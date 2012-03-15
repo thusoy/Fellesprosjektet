ceb6f7838d901060cc01474cc9b61c032f193c78
Tarjei Husoy <tarjei@roms.no>
2012-03-15 14:55:15 +0100

    La inn constructor.

--- Fellesprosjektet/src/calendar/Room.java
+++ Fellesprosjektet/src/calendar/Room.java
@@ -8,18 +8,24 @@ public class Room extends DBObject<Room>{
 	private int capacity;
 	private LinkedHashMap<Date, Date> isOccupied;
 	
+	public Room(String name, int capacity){
+		this.name = name;
+		this.capacity = capacity;
+		isOccupied = new LinkedHashMap<Date, Date>();
+	}
+	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public int getCapacity() {
 		return capacity;
 	}
+	
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
+
 	public LinkedHashMap<Date, Date> getIsOccupied() {
 		return isOccupied;
 	}
