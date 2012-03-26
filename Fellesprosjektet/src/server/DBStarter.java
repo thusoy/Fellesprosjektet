f63d55a89a7282e762cfef802bc0962655fa32dc
Tarjei Husoy <tarjei@roms.no>
2012-03-26 15:49:05 +0200

    Miniendring.

--- Fellesprosjektet/src/server/DBStarter.java
+++ Fellesprosjektet/src/server/DBStarter.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.net.InetAddress;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -18,7 +19,8 @@ public class DBStarter extends RmiStarter {
 		try {
             DBAccess engine = new AppointmentHandler();
             DBAccess engineStub = (DBAccess) UnicastRemoteObject.exportObject(engine, 0);
-
+            InetAddress addr = InetAddress.getLocalHost();
+            System.out.println("RMI server address: " + addr.getHostAddress());
             LocateRegistry.createRegistry(1099);
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind(AppointmentHandler.SERVICE_NAME, engineStub);
